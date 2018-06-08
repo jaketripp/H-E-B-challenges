0a001964cbc117e6f51d1cac071ce1e86e7e6c15
Jacob Tripp <jake@Shimmer.local>
2018-06-08 14:08:32 -0500

    Break header and footer into separate components

---
+++ dev_2_challenge/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <p>H-E-B Developer II Challenge</p>
+      <p>Made by <a href="https://jaketripp.com">Jake Tripp</a> 2018</p>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ dev_2_challenge/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div id="header">
+      <img src="/images/logo.png" alt="HEB logo" />
+      <h1>H-E-B Product Search</h1>
+    </div>
+  )
+}
+
+export default Header;
