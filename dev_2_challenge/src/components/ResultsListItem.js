381fdd6f810564e4f09da3b5daecb8a58c8eafbd
Jacob Tripp <jake@Shimmer.local>
2018-06-08 18:02:58 -0500

    Make details paragraph tags. Prettier

--- dev_2_challenge/src/components/ResultsListItem.js
+++ dev_2_challenge/src/components/ResultsListItem.js
@@ -2,17 +2,39 @@ import React from "react";
 
 function ResultsListItem(props) {
   let { product } = props;
-  let { Description, Price, xFor, Unit } = product;
+  let {
+    ID,
+    Description,
+    lastSold,
+    ShelfLife,
+    Department,
+    Price,
+    xFor,
+    Unit,
+    Cost
+  } = product;
   return (
-    <li>
-      <div>{toTitleCase(Description)}</div>
-      <div>{Price} | {xFor} {Unit}</div>
-    </li>
+    <div className="card">
+      <div className="card__header">
+        <p className="product-name">{toTitleCase(Description)}</p>
+        <p>
+          {Price} - {xFor} {Unit}
+        </p>
+      </div>
+      <hr/>
+      <div className="card__details">
+        <p>ID: #{ID}</p>
+        <p>Department: {Department}</p>
+        <p>Last Sold: {lastSold}</p>
+        <p>Shelf Life: {ShelfLife}</p>
+        <p>Cost: {Cost}</p>
+      </div>
+    </div>
   );
 }
 
 function toTitleCase(string) {
-  return string.replace(/\w\S*/g, function (txt) {
+  return string.replace(/\w\S*/g, function(txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
