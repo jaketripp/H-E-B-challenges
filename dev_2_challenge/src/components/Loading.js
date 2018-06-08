36ac45981c421e1e23a2fa86ee95ae1850ded183
Jacob Tripp <jake@Shimmer.local>
2018-06-08 14:59:34 -0500

    Add Loading spinner

---
+++ dev_2_challenge/src/components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => (
+  <div className="loader">
+    <img className="loader__image" src="/images/loader.gif" alt="Loading wheel" />
+  </div>
+);
+
+export default Loading;
