f28c4079881610c1ec5f1b189577b2312d238ddb
Jacob Tripp <jake@Shimmer.local>
2018-06-08 18:01:39 -0500

    Refactor into functional component

--- dev_2_challenge/src/components/ResultsList.js
+++ dev_2_challenge/src/components/ResultsList.js
@@ -1,38 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import ResultsListItem from "./ResultsListItem";
 
-class ResultsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...props
-    };
-  }
-
-  // componentWillReceiveProps(nextProps) {
-  //   // You don't have to do this check first, but it can help prevent an unneeded render
-  //   if (nextProps.results !== this.state.results) {
-  //     this.setState({ results: nextProps.results });
-  //   }
-  // }
-
-  render(props) {
-    console.log(this.state.results);
-    return (
+const ResultsList = props => {
+  return (
+    <div className="results-list-container">
       <div className="results-list">
-        {this.props.results.map((product, i) => {
-          // let selectedPersonName = props.selectedPersonInfo.name;
-          return (
-            <ResultsListItem
-              key={i}
-              {...props}
-              product={product}
-            />
-          );
+        {props.results.map((product, i) => {
+          return <ResultsListItem key={i} {...props} product={product} />;
         })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ResultsList;
