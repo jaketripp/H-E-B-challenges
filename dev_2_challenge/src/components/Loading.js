f9d743760fcaa159e6a770165c7bc33d979565ad
Jacob Tripp <jake@Shimmer.local>
2018-06-08 16:58:04 -0500

    Remove spinner

--- dev_2_challenge/src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Loading = () => (
-  <div className="loader">
-    <img className="loader__image" src="/images/loader.gif" alt="Loading wheel" />
-  </div>
-);
-
-export default Loading;
