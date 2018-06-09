bb5d2d5c00a67d798c52506836da249efcf445ca
Jacob Tripp <jake@Shimmer.local>
2018-06-09 13:55:47 -0500

    Break styles into partials and have one styles.js that imports them

--- dev_2_challenge/src/App.js
+++ dev_2_challenge/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import ResultsList from "./components/ResultsList";
---
+++ dev_2_challenge/src/styles.js
@@ -0,0 +1,6 @@
+import './styles/normalize.css';
+import './styles/base.css';
+import './styles/Header.css';
+import './styles/SearchBar.css';
+import './styles/ResultsList.css';
+import './styles/Footer.css';
